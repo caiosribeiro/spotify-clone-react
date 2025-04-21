4b59a99121ed1c9282f8767f2cd589a67fc4874a
Caio Ribeiro <51218509+caiosribeiro@users.noreply.github.com>
2025-04-21 11:27:12 -0300

    Update connect.js

--- connect.js
+++ connect.js
@@ -3,5 +3,6 @@ import { MongoClient } from "mongodb";
 const URI = process.env.MONGO_URI;
 
 const client = new MongoClient(URI);
+await client.connect();
 
 export const db = client.db("spotifyAula");
